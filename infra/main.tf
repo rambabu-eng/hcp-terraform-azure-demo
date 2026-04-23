41ce8d72573435746005e559e901b2f9afb62565
Rambabu Katta <rambabu-eng-ram.analytical@gmail.com>
2026-04-23 16:48:26 +1000

    Infra file added

---
+++ infra/main.tf
@@ -0,0 +1,40 @@
+terraform {
+  required_version = ">=1.5"
+
+  cloud {
+    organization = "rambabu-cloud-lab"
+
+    workspaces {
+      name = "azure-dev"
+    }
+  }
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~>4.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+resource "azurerm_resource_group" "rg" {
+  name     = "rg-hcp-demo"
+  location = "Australia East"
+}
+
+resource "azurerm_storage_account" "st" {
+  name                     = "ramst${random_string.rand.result}"
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+resource "random_string" "rand" {
+  length  = 6
+  special = false
+  upper   = false
+}
